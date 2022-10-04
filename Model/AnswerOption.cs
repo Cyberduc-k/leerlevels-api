3f070e28c1c35d0835add9eec53916b1b6e29d99
Cyberduc-k <tttymo@gmail.com>
2022-10-04 14:33:33 +0200

    add Mcq and AnswerOption models

---
+++ Model/AnswerOption.cs
@@ -0,0 +1,21 @@
+﻿namespace Model;
+
+public class AnswerOption
+{
+    public string Id { get; set; }
+    public int Index { get; set; }
+    public string Text { get; set; }
+    public bool IsCorrect { get; set; }
+
+    public AnswerOption()
+    {
+    }
+
+    public AnswerOption(string id, int index, string text, bool isCorrect)
+    {
+        Id = id;
+        Index = index;
+        Text = text;
+        IsCorrect = isCorrect;
+    }
+}
---
+++ Model/Mcq.cs
@@ -0,0 +1,25 @@
+﻿namespace Model;
+
+public class Mcq
+{
+    public string Id { get; set; }
+    public Target Target { get; set; }
+    public string QuestionText { get; set; }
+    public string Explanation { get; set; }
+    public bool AllowRandom { get; set; }
+    public virtual ICollection<AnswerOption> AnswerOptions { get; set; }
+
+    public Mcq()
+    {
+    }
+
+    public Mcq(string id, Target target, string questionText, string explanation, bool allowRandom, ICollection<AnswerOption> answerOptions)
+    {
+        Id = id;
+        Target = target;
+        QuestionText = questionText;
+        Explanation = explanation;
+        AllowRandom = allowRandom;
+        AnswerOptions = answerOptions;
+    }
+}
