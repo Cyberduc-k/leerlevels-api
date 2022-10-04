4c85bde5ef546ae78c20c84015c41929855c0848
Cyberduc-k <tttymo@gmail.com>
2022-10-04 16:29:00 +0200

    add data layer for entity framework

---
+++ Data/TargetContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace Data;
+
+public class TargetContext : DbContext
+{
+    public DbSet<Target> Targets { get; set; }
+    public DbSet<Mcq> Mcqs { get; set; }
+    public DbSet<AnswerOption> AnswerOptions { get; set; }
+}
