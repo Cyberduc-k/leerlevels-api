7ecb8544a382e3ff23aa3fc23e187fc1bf946c08
Cyberduc-k <tttymo@gmail.com>
2022-10-10 11:43:50 +0200

    add Forum DTOs and Replies

---
+++ Model/DTO/ForumDTO.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace Model.DTO;
+
+public class ForumDTO
+{
+    [JsonRequired]
+    public string Title { get; set; }
+
+    [JsonRequired]
+    public string Description { get; set; }
+
+    [JsonRequired]
+    public string FromId { get; set; }
+
+    public ForumDTO()
+    {
+    }
+
+    public ForumDTO(string title, string description, string fromId)
+    {
+        Title = title;
+        Description = description;
+        FromId = fromId;
+    }
+}
---
+++ Model/DTO/ForumReplyDTO.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Model.DTO;
+
+public class ForumReplyDTO
+{
+    [JsonRequired]
+    public string FromId { get; set; }
+
+    [JsonRequired]
+    public string Text { get; set; }
+
+    public ForumReplyDTO()
+    {
+    }
+
+    public ForumReplyDTO(string fromId, string text)
+    {
+        FromId = fromId;
+        Text = text;
+    }
+}
