997efab3d2afa16fd72486f0e825c727a9668027
Cyberduc-k <tttymo@gmail.com>
2022-10-13 18:13:16 +0200

    add more unit tests for ForumService

--- Service.Test/ForumServiceTests.cs
+++ Service.Test/ForumServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using Model;
+using Model.DTO;
 using Moq;
 using Repository.Interfaces;
 using Service.Exceptions;
@@ -21,6 +22,37 @@ public class ForumServiceTests
     }
 
     [Fact]
+    public async Task GetAllReturnsArray()
+    {
+        async IAsyncEnumerable<Forum> MockForums()
+        {
+            yield return new Forum("1", "Test Forum 1", "test", null!, null!);
+            yield return new Forum("2", "Test Forum 2", "test test", null!, null!);
+        }
+
+        _mockRepository.Setup(r => r.GetAllAsync()).Returns(() => MockForums());
+
+        ICollection<Forum> forums = await _service.GetAll();
+
+        Assert.Equal(2, forums.Count);
+    }
+
+    [Fact]
+    public async Task CreateForumReturnsForum()
+    {
+        _mockRepository.Setup(r => r.InsertAsync(It.IsAny<Forum>())).Verifiable();
+        _mockRepository.Setup(r => r.SaveChanges()).Verifiable();
+
+        Forum newForum = new();
+        Forum forum = await _service.CreateForum(newForum);
+
+        Assert.NotNull(forum.Id);
+
+        _mockRepository.Verify(r => r.InsertAsync(It.IsAny<Forum>()), Times.Once);
+        _mockRepository.Verify(r => r.SaveChanges(), Times.Once);
+    }
+
+    [Fact]
     public async Task GetByIdReturnsForum()
     {
         _mockRepository.Setup(r => r.GetByIdAsync("1")).ReturnsAsync(() => new Forum("1", "Test Forum", "test test test", null!, null!));
@@ -35,5 +67,37 @@ public class ForumServiceTests
         _mockRepository.Setup(r => r.GetByIdAsync(It.IsNotIn("1"))).ReturnsAsync(() => null);
 
         Assert.ThrowsAsync<NotFoundException>(() => _service.GetById("fdsfdfs"));
+    }
+
+    [Fact]
+    public async Task GetReplyByIdReturnsForumReply()
+    {
+        _mockReplyRepository.Setup(r => r.GetByIdAsync("1")).ReturnsAsync(() => new ForumReply("1", null!, "Test Forum", 0));
+        ForumReply reply = await _service.GetReplyById("1");
+
+        Assert.Equal("1", reply.Id);
+    }
+
+    [Fact]
+    public void GetReplyByIdThrowsNotFoundException()
+    {
+        _mockReplyRepository.Setup(r => r.GetByIdAsync(It.IsNotIn("1"))).ReturnsAsync(() => null);
+
+        Assert.ThrowsAsync<NotFoundException>(() => _service.GetReplyById("fdsfdfs"));
+    }
+
+    [Fact]
+    public async Task UpdateForumHasPropertyChanged()
+    {
+        Forum forum = new("1", "Test Title", "test test test", null!, null!);
+
+        _mockRepository.Setup(r => r.GetByIdAsync("1")).ReturnsAsync(() => forum);
+        _mockRepository.Setup(r => r.SaveChanges()).Verifiable();
+
+        UpdateForumDTO changes = new() { Title = "New Test Title" };
+        await _service.UpdateForum("1", changes);
+
+        Assert.Equal("New Test Title", forum.Title);
+        _mockRepository.Verify(r => r.SaveChanges(), Times.Once);
     }
 }
