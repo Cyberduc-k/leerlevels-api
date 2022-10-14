72620fb2dd6b6a10e61d24a83ecf93917872781d
Elias Al Khouri Hanna <eleaghanem8@gmail.com>
2022-10-14 22:10:48 +0200

    0Add Target And Mcq Test For Service Layer

---
+++ Service.Test/TargetServiceTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Model;
+using Moq;
+using Repository.Interfaces;
+using Service.Exceptions;
+using Xunit;
+
+namespace Service.Test;
+public class TargetServiceTests
+{
+    private readonly Mock<ITargetRepository> _mockRepository;
+    private readonly TargetService _service;
+
+    public TargetServiceTests()
+    {
+        _mockRepository = new();
+        _service = new TargetService(new LoggerFactory(), _mockRepository.Object);
+    }
+
+    [Fact]
+    public async Task Get_All_Targets_Should_return_an_array_of_targets()
+    {
+
+        async IAsyncEnumerable<Target> MockTargets()
+        {
+            yield return new Target( "1", "Lading concept",
+                "Je kan in eigen woorden uitleggen welk effect lading kan hebben.",
+                "Lading is een eigenschap die bepaalt hoe een deeltje wordt beïnvloed door een elektrisch of magnetisch veld.",
+                "0ouf-xbz7_o",
+               "https://s3-us-west-2.amazonaws.com/leerlevels/slide_pngs/2.png", null!);
+
+            yield return new Target("2", "Lading concept",
+                  "Je kan in eigen woorden uitleggen welk effect lading kan hebben.",
+                  "Lading is een eigenschap die bepaalt hoe een deeltje wordt beïnvloed door een elektrisch of magnetisch veld.",
+                  "0ouf-xbz7_o",
+                 "https://s3-us-west-2.amazonaws.com/leerlevels/slide_pngs/2.png", null!);
+
+        }
+
+        _mockRepository.Setup(r => r.GetAllAsync()).Returns(MockTargets);
+
+        ICollection<Target> mcqs = await _service.GetAllTargetsAsync();
+
+        Assert.Equal(2, mcqs.Count);
+    }
+    [Fact]
+    public async Task Get_Target_By_Id_Should_return_A_Target_Object()
+    {
+        _mockRepository.Setup(r => r.GetByIdAsync("1")).ReturnsAsync(() => new Target("1", "Lading concept",
+                  "Je kan in eigen woorden uitleggen welk effect lading kan hebben.",
+                  "Lading is een eigenschap die bepaalt hoe een deeltje wordt beïnvloed door een elektrisch of magnetisch veld.",
+                  "0ouf-xbz7_o",
+                 "https://s3-us-west-2.amazonaws.com/leerlevels/slide_pngs/2.png", null!));
+        Target target = await _service.GetTargetByIdAsync("1");
+
+        Assert.Equal("1", target.Id);
+    }
+
+    [Fact]
+    public void Get_Target_By_Id_should_Throws_Not_Found_Exception()
+    {
+        _mockRepository.Setup(r => r.GetByIdAsync(It.IsNotIn("1"))).ReturnsAsync(() => null);
+        Assert.ThrowsAsync<NotFoundException>(() => _service.GetTargetByIdAsync("3"));
+    }
+
+    [Fact]
+    public void Get_Target_By_Id_should_Throws_null_Exception()
+    {
+        _mockRepository.Setup(r => r.GetByIdAsync(It.IsNotIn(""))).ReturnsAsync(() => null);
+        Assert.ThrowsAsync<NullReferenceException>(() => _service.GetTargetByIdAsync(""));
+    }
+}
