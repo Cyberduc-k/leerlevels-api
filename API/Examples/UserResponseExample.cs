7bd24498d5392efe2166e7c518288bd75385e698
Sander <thesannio123@gmail.com>
2022-10-20 20:02:17 +0200

    added OpenAPI package to models for usable example swagger schemas
    The plan is to use the models themselves to add openAPI properties to so the schemas can be used nicely as a proper form of documentation for our api from the azure function API application.

--- API/Examples/UserResponseExample.cs
+++ API/Examples/UserResponseExample.cs
@@ -2,14 +2,15 @@
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Resolvers;
 using Microsoft.OpenApi.Models;
 using Model;
+using Model.Response;
 using Newtonsoft.Json.Serialization;
 
 namespace API.Examples;
-public class UserResponseExample : OpenApiExample<UserResponseOpenApiExample>
+public class UserResponseExample : OpenApiExample<UserResponse>
 {
-    public override IOpenApiExample<UserResponseOpenApiExample> Build(NamingStrategy namingStrategy)
+    public override IOpenApiExample<UserResponse> Build(NamingStrategy namingStrategy)
     {
-        Examples.Add(OpenApiExampleResolver.Resolve("Jan", new UserResponseOpenApiExample() { Id = "a75e3fe7-f519-48de-a106-79f788a1b479", Email = "jan@gmail.com", FirstName = "Jan", LastName = "Groothuis", UserName = "", Role = UserRole.Student, ShareCode = "DTRY-WQER-PIGU-VNSA" }, namingStrategy));
+        Examples.Add(OpenApiExampleResolver.Resolve("Jan", new UserResponse() { Id = "a75e3fe7-f519-48de-a106-79f788a1b479", Email = "jan@gmail.com", FirstName = "Jan", LastName = "Groothuis", UserName = "JanG#1", Role = UserRole.Student, ShareCode = "DTRY-WQER-PIGU-VNSA" }, namingStrategy));
 
         return this;
     }
--- Model/DTO/UpdateUserDTO.cs
+++ Model/DTO/UpdateUserDTO.cs
@@ -1,10 +1,19 @@
-﻿namespace Model.DTO;
+﻿using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+
+namespace Model.DTO;
 
 public class UpdateUserDTO
 {
+    [OpenApiProperty(Default = "email@mail.com", Description = "The new email address for a user to be updated", Nullable = true)]
     public string? Email {  get; set; }
+
+    [OpenApiProperty(Default = "John", Description = "The new first name of a user", Nullable = true)]
     public string? FirstName { get; set; }
+
+    [OpenApiProperty(Default = "Doe", Description = "The new last name of a user", Nullable = true)]
     public string? LastName { get; set; }
+
+    [OpenApiProperty(Default = "JohnnyD", Description = "The new username for a user", Nullable = true)]
     public string? UserName { get; set; }
 
     //public string? Password { get; set; }
