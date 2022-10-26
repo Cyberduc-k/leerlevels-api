3e033162980c6e8528df0b42b097ef10a29e3050
Cyberduc-k <tttymo@gmail.com>
2022-10-26 20:33:09 +0200

    add more tests for BookmarkController

--- API.Test/BookmarkControllerTests.cs
+++ API.Test/BookmarkControllerTests.cs
@@ -6,6 +6,8 @@ using AutoMapper;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using Model;
+using Model.DTO;
+using Model.Response;
 using Moq;
 using Service.Interfaces;
 using Xunit;
@@ -31,6 +33,11 @@ public class BookmarkControllerTests
         _tokenService.SetupGet(s => s.User).Returns(new User() { Id = "1", Role = UserRole.Student });
     }
 
+    private string GetHeaderValue(HttpResponseData resp, string headerName)
+    {
+        return resp.Headers.First(h => h.Key == headerName).Value.First();
+    }
+
     [Fact]
     public async Task Get_Bookmarks_Should_Respond_OK()
     {
@@ -42,5 +49,37 @@ public class BookmarkControllerTests
         HttpResponseData response = await _controller.GetBookmarks(request);
 
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Get_Bookmarks_Should_Respond_Json()
+    {
+        _bookmarkService
+            .Setup(s => s.GetBookmarksAsync(It.IsAny<User>()))
+            .ReturnsAsync(() => (
+                new Target[] { new() { Id = "1", Label = "Label", Description = "Description" } },
+                new Mcq[] { new() { Id = "1", QuestionText = "QuestionText" } }
+            ));
+
+        HttpRequestData request = MockHelpers.CreateHttpRequestData();
+        HttpResponseData response = await _controller.GetBookmarks(request);
+        BookmarksResponse? result = await response.ReadFromJsonAsync<BookmarksResponse>();
+
+        Assert.Equal("application/json; charset=utf-8", GetHeaderValue(response, "Content-Type"));
+        Assert.NotNull(result);
+        Assert.Equal(1, result!.Targets.Count);
+        Assert.Equal(1, result!.Mcqs.Count);
+    }
+
+    [Fact]
+    public async Task Add_Bookmark_Should_Respond_OK()
+    {
+        _bookmarkService.Setup(s => s.AddBookmark(It.IsAny<User>(), It.IsAny<Bookmark>())).Verifiable();
+
+        HttpRequestData request = MockHelpers.CreateHttpRequestData(new BookmarkDTO("2", Bookmark.BookmarkType.Mcq));
+        HttpResponseData response = await _controller.AddBookmark(request);
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        _bookmarkService.Verify(s => s.AddBookmark(It.IsAny<User>(), It.IsAny<Bookmark>()), Times.Once);
     }
 }
--- API.Test/Mocks/MockHelpers.cs
+++ API.Test/Mocks/MockHelpers.cs
@@ -3,12 +3,20 @@ using Azure.Core.Serialization;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 namespace API.Test.Mocks;
 
 // taken from: https://github.com/lohithgn/az-fx-isolated-unittest/blob/main/test/Microsoft.Azure.Functions.Isolated.TestDoubles/MockHelpers.cs
 public static class MockHelpers
 {
+    public static HttpRequestData CreateHttpRequestData<T>(T payload, string? token = null, string method = "GET")
+    {
+        string json = JsonConvert.SerializeObject(payload);
+
+        return CreateHttpRequestData(json, token, method);
+    }
+
     public static HttpRequestData CreateHttpRequestData(string? payload = null, string? token = null, string method = "GET")
     {
         string input = payload ?? string.Empty;
