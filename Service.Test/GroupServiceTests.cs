c53886258a896808f9e4fbc4dc9d96243f1ae33a
Cyberduc-k <tttymo@gmail.com>
2022-11-20 14:02:00 +0100

    fix group service tests

--- Service.Test/GroupServiceTests.cs
+++ Service.Test/GroupServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Linq.Expressions;
+using Microsoft.Extensions.Logging;
 using Model;
 using Moq;
 using Repository.Interfaces;
@@ -25,7 +26,7 @@ public class GroupServiceTests
             new Group("2", "second bestgroup", "this is the rest of the groups", EducationType.Havo, SchoolYear.One, null!, null!),
         };
 
-        _groupRepository.Setup(r => r.Include(x => x.Sets).Include(x => x.Users).GetAllAsync()).Returns(mockGroups.ToAsyncEnumerable());
+        _groupRepository.Setup(r => r.GetAllAsync()).Returns(mockGroups.ToAsyncEnumerable());
 
         ICollection<Group> groups = await _service.GetAllGroupsAsync();
 
@@ -35,14 +36,15 @@ public class GroupServiceTests
     [Fact]
     public async Task Get_Group_By_Id_Should_return_A_Group_Object()
     {
-        _groupRepository.Setup(r => r.GetByIdAsync("1")).ReturnsAsync(() => new Group("1",
+        string groupId = "1";
+        _groupRepository.Setup(r => r.Include(g => g.Sets).Include(g => g.Users).GetByAsync(g => g.Id == groupId)).ReturnsAsync(() => new Group("1",
            "second bestgroup",
            "this is the rest of the groups",
            EducationType.Havo,
            SchoolYear.One,
            null!,
            null!));
-        Group group = await _service.GetGroupByIdAsync("1");
+        Group group = await _service.GetGroupByIdAsync(groupId);
 
         Assert.Equal("1", group.Id);
     }
@@ -50,14 +52,14 @@ public class GroupServiceTests
     [Fact]
     public void Get_Group_By_Id_should_Throws_Not_Found_Exception()
     {
-        _groupRepository.Setup(r => r.GetByIdAsync(It.IsNotIn("1"))).ReturnsAsync(() => null);
+        _groupRepository.Setup(r => r.Include(g => g.Sets).Include(g => g.Users).GetByAsync(It.IsAny<Expression<Func<Group, bool>>>())).ReturnsAsync(() => null);
         Assert.ThrowsAsync<NotFoundException>(() => _service.GetGroupByIdAsync("3"));
     }
 
     [Fact]
     public void Get_Group_By_Id_should_Throws_null_Exception()
     {
-        _groupRepository.Setup(r => r.GetByIdAsync(It.IsNotIn(""))).ReturnsAsync(() => null);
+        _groupRepository.Setup(r => r.Include(g => g.Sets).Include(g => g.Users).GetByAsync(It.IsAny<Expression<Func<Group, bool>>>())).ReturnsAsync(() => null);
         Assert.ThrowsAsync<NullReferenceException>(() => _service.GetGroupByIdAsync(""));
     }
 }
