af08c194725b6de5c7b1c1eabbb7d448452e241f
Sander <thesannio123@gmail.com>
2022-12-15 16:25:12 +0100

    update token duration
    note to change this back later I guess

--- Service/TokenService.cs
+++ Service/TokenService.cs
@@ -41,7 +41,9 @@ public class TokenService : ITokenService
         Issuer = "LeerLevels";
         Audience = "Users of the LeerLevels applications";
 
-        ValidityDuration = TimeSpan.FromHours(2);
+        //ValidityDuration = TimeSpan.FromHours(2);
+        ValidityDuration = TimeSpan.FromDays(1);
+
 
         string Key = Environment.GetEnvironmentVariable("LeerLevelsTokenKey")!;
 
@@ -97,13 +99,16 @@ public class TokenService : ITokenService
         };
 
         if (refreshTokenPhrase == "yes") {
-            ValidityDuration = TimeSpan.FromHours(2.25); //initial refresh token is valid for 15 minutes longer than the initial token to allow for a call to refresh this token
+            //ValidityDuration = TimeSpan.FromHours(2.25); //initial refresh token is valid for 15 minutes longer than the initial token to allow for a call to refresh this token
+            ValidityDuration = TimeSpan.FromDays(1.25);
             Claims.Add(new Claim("initTokenExpiredAt", DateTime.UtcNow.Add(ValidityDuration).ToString()));
         } else if (refreshTokenPhrase == "refresh") {
-            ValidityDuration = TimeSpan.FromHours(2.25);
+            //ValidityDuration = TimeSpan.FromHours(2.25);
+            ValidityDuration = TimeSpan.FromDays(1.25);
             Claims.Add(new Claim("initTokenExpiredAt", initialTokenExpiration));
         } else if (refreshTokenPhrase == "no") {
-            ValidityDuration = TimeSpan.FromHours(2);
+            ValidityDuration = TimeSpan.FromDays(1);
+            //ValidityDuration = TimeSpan.FromHours(2);
         }
 
         JwtPayload Payload = new(
